a8dc3f2cd2bbadebd344e010559af100974568c7
magdmagd <dv.aboelmagd@gmail.com>
2020-12-29 15:52:04 +0200

    Update Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {  handleInitialData} from '../actions/shared'
 
 
-function App() {
+class App extends React.Component
+ {
+  componentDidMount () 
+  {
+    const { dispatch } = this.props
+    dispatch(handleInitialData())
+  }
+  render() 
+  {
+    if (this.props.loading === true) 
+    {
+      return <h3>Loading</h3>
+    }
+
+    return (
+      <div>
+        Hello World
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    )
+  }
+}
+
+
+/*function App() {
   return (
     <div className="App">
       Hello World       
     </div>
   );
-}
+}*/
 
-export default App;
+//export default App;
+export default connect((state) => ({  loading: state.loading}))(App) 
