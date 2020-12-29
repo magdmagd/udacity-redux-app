daeb868c1daff64e5b8ee74bfe071e1aaaf5fabc
magdmagd <dv.aboelmagd@gmail.com>
2020-12-29 16:01:03 +0200

    Create Main store

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -3,7 +3,7 @@ import API from 'goals-todos-api'
 
 export const ADD_TODO = 'ADD_TODO'
 export const REMOVE_TODO = 'REMOVE_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO 
+export const TOGGLE_TODO = 'TOGGLE_TODO' 
 
 function addTodo (todo) 
 {
